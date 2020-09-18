9c382018239c3e97a9f75ab66e2abde70ed253b6
xlakostax <konstantin.veselovskii@gmail.com>
2020-09-18 09:46:54 +0300

    Minor fixes

--- src/EventsList.js
+++ src/EventsList.js
@@ -2,43 +2,31 @@ import React, {useEffect, useState} from 'react';
 
 import axios from 'axios';
 import debounce from "lodash.debounce";
-// import { stringify } from 'query-string';
 import { Events, animateScroll as scroll} from 'react-scroll'
 
 /*Import components*/
 import EventBox from './components/EventBox';
 import Header from './components/Header';
-import MapContainer from './Map/MapContainer';
-import Navigation from './components/Navigation';
+// import MapContainer from './Map/MapContainer';
+// import Navigation from './components/Navigation';
 
-
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("back-to-top").style.display = "block";
-  } else {
-    document.getElementById("back-to-top").style.display = "none";
-  }
-}
 
 const EventsList = () => {
   const [data, setData] = useState([])
-  console.log(data.length);
-  let limit = 2;
-  let start = data.length;
-  const [filter, setFilter] = useState({
-    filter_lang: "",
-    tags_filter: "",
-  })
-  const [pins, setPins] = useState([])
-  const [selectedEvent, setSelectedEvent] = useState([])
-
-  const [scroll, setScroll] = useState({
+  // const [filter, setFilter] = useState({
+  //   filter_lang: "",
+  //   tags_filter: "",
+  // });
+  // const [pins, setPins] = useState([]);
+  // const [selectedEvent, setSelectedEvent] = useState([]);
+  const [scrolling, setScrolling] = useState({
     error: false,
     hasMore: true,
     isLoading: false,
   });
-
-  const [event, setEvent] = ("");
+  // const [event, setEvent] = ("");
+  let limit = 2;
+  let start = data.length;
 
   // const getPins = () => {
   //   axios.get(`/api/pins/?${this.state.filter_type}&${this.state.filter_lang}`)
@@ -68,45 +56,50 @@ const EventsList = () => {
   //   });
   // }
 
-  const load = async () => {
+  const getEvents = async () => {
     return (await axios.get(`https://api-inhelsinki.herokuapp.com/api/v1/events/?limit=${limit}&start=${start}`)).data.data
-    // return (await axios.get(`https://cors-anywhere.herokuapp.com/http://open-api.myhelsinki.fi/v1/events/?limit=${limit}&start=${start}`)).data.data
   }
 
-  load().then(res => console.log(res))
+  // getEvents().then(res => console.log(res))
 
   const updateState = () => {
-    load(limit, start)
+    getEvents(limit, start)
     .then(res => {
-      setScroll({
-        ...scroll,
+      setScrolling({
+        ...scrolling,
         isLoading: true,
       });
       setData([...data, ...res])
-      setScroll({
-        ...scroll,
+      setScrolling({
+        ...scrolling,
         hasMore: res.length !== 0,
         isLoading: false,
       });
     })
     .catch(err => console.log(err));
+  };
+
+  const showToTopButton = () => {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+      document.getElementById("back-to-top").style.display = "block";
+    } else {
+      document.getElementById("back-to-top").style.display = "none";
+    }
   }
 
   window.onscroll = debounce(() => {
-    scrollFunction();
-    const { error, isLoading, hasMore } = scroll;
+    showToTopButton();
+    const { error, isLoading, hasMore } = scrolling;
     if (error || isLoading || !hasMore) {
-      console.log(error, isLoading, hasMore);
-      return "Hi!"
+      return
     };
     if (
       window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight
     ) {
-      console.log(error, isLoading, hasMore);
-      console.log("window.innerHeight: ", window.innerHeight)
-      console.log("document.documentElement.scrollTop: ", document.documentElement.scrollTop)
-      console.log("document.documentElement.offsetHeight: ", document.documentElement.offsetHeight)
+      // console.log("window.innerHeight: ", window.innerHeight)
+      // console.log("document.documentElement.scrollTop: ", document.documentElement.scrollTop)
+      // console.log("document.documentElement.offsetHeight: ", document.documentElement.offsetHeight)
       updateState();
     } else {
       return;
@@ -114,9 +107,6 @@ const EventsList = () => {
   }, 100);
 
   useEffect(() => {
-    // getEvents();
-    // getPins();
-    // window.addEventListener('scroll', handleScroll);
     Events.scrollEvent.register('begin', function () {});
     Events.scrollEvent.register('end', function () {});
     updateState();
@@ -129,10 +119,10 @@ const EventsList = () => {
   return (
     <div className='App'>
       <Header />
-      <Navigation
-        onChange={{/*handleChange*/}}
+      {/* <Navigation
+        //onChange={handleChange}
         // onClick={handleSubmit}
-      />
+      /> */}
       <div className='map-events-holder'>
         <article>
           {data.map((el, index) => {
